323a2ca8ed113fa5a159683aaa4280934c7c83d1
=Abdeljabar <dev.taoufikallah@gmail.com>
2018-07-13 09:48:54 +0100

    updated User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id()
@@ -26,6 +27,15 @@ class User
      */
     private $password;
 
+    private $plainPassword;
+
+    /**
+     * @return array
+     *
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
     public function getId()
     {
         return $this->id;
@@ -43,6 +53,30 @@ class User
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param mixed $plainPassword
+     *
+     * @return string
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+
+        if ($plainPassword !== null) {
+            $this->setPassword($plainPassword);
+        }
+
+        return $this;
+    }
+
     public function getPassword(): ?string
     {
         return $this->password;
@@ -53,5 +87,67 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    public function getRoles(): array {
+        $roles = $this->roles;
+
+        if (empty($roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return $roles;
+    }
+
+    public function setRoles(array $roles): self {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function serialize() {
+        return serialize([
+            $this->id,
+            $this->email,
+            $this->password
+        ]);
+    }
+
+    public function unserialize($serialized) {
+        list($this->id, $this->email, $this->password) = unserialize($serialized);
+    }
+
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        return null;
     }
 }
